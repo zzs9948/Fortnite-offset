e9b97fb65ec64b23d480129c83a64f1f5a064904
DerutaBuilder <111275501+DerutaBuilder@users.noreply.github.com>
2022-08-17 04:11:34 +0000

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -6,18 +6,18 @@
 
 namespace OFFSETS
 {
-    uintptr_t Gameinstance = 0x1A8; //UWorld OwningGameInstance
-    uintptr_t LocalPlayers = 0x38; //UGameInstance
-    uintptr_t PlayerController = 0x30; //UPlayer
+    uintptr_t Gameinstance = 0x1A8; 
+    uintptr_t LocalPlayers = 0x38; 
+    uintptr_t PlayerController = 0x30; 
     uintptr_t LocalPawn = 0x310;
-    uintptr_t PlayerState = 0x290; //APawn
-    uintptr_t RootComponet = 0x188; //AActor
-    uintptr_t PersistentLevel = 0x30; //UWorld
+    uintptr_t PlayerState = 0x290; 
+    uintptr_t RootComponet = 0x188; 
+    uintptr_t PersistentLevel = 0x30;
     uintptr_t ActorCount = 0xA0;
     uintptr_t AActor = 0x98;
     uintptr_t CurrentActor = 0x8;
-    uintptr_t Mesh = 0x2F0; //ACharacter
-    uintptr_t TeamId = 0x1048; //AFortPlayerStateAthena
+    uintptr_t Mesh = 0x2F0;
+    uintptr_t TeamId = 0x1048; 
     uintptr_t LocalActorPos = 0x128;
     uintptr_t ComponetToWorld = 0x240;
     uintptr_t BoneArray = 0x590;
